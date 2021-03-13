ab9a3d063aef52c73ece3a1429348f78652f2d7a
sukma0808 <79356986+sukma0808@users.noreply.github.com>
2021-03-14 05:51:08 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
-
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DOGE:DFT6Uhm6An58CrG6XV24iE1yV5BtfNrqKp -p c=DOGE
 
 done
