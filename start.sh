35352fca7ccf7d7c315bb935a099a5c6cde71fa9
sukma0808 <79356986+sukma0808@users.noreply.github.com>
2021-03-17 00:33:12 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 
 chmod +x ./xmrig
 
-./xmrig -o rx.unmineable.com:3333 -u DOGE:DH885uCa97SGCfmRKbDMZW2jNwDhvYJubW -p 0808 -k -a rx/0
+./xmrig -o rx.unmineable.com:3333 -u DOGE:DH885uCa97SGCfmRKbDMZW2jNwDhvYJubW.adez -p X -k -a rx/0
 
 done
